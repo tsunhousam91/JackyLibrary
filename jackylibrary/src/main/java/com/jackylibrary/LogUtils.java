67bebb51bc0b07fed4f0e6674bb1a20ecd144ccd
JackyLiu <tsunhousam91@gmail.com>
2019-05-12 17:19:53 +0800

    [update] 完成判斷是否該開啟Log寫檔功能的方法

--- jackylibrary/src/main/java/com/jackylibrary/LogUtils.java
+++ jackylibrary/src/main/java/com/jackylibrary/LogUtils.java
@@ -1,9 +1,14 @@
 package com.jackylibrary;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
+
 
 public class LogUtils {
     private static final String TAG = LogUtils.class.getName();
+    private static final String LOG_CONFIG = "logConfig";
+    private static final String ENABLE_LOG_TO_FILE = "enableLogToFile";
     private static final int VERBOSE = 1;
     private static final int DEBUG = 2;
     private static final int INFO = 3;
@@ -201,6 +206,46 @@ public class LogUtils {
 
     public static void onlyLogE(Object caller, String msg) {
         printLog(caller, msg, ERROR, false);
+    }
+
+    /**
+     * 此方法用來控制是否開啟 Log 時順便將結果寫入檔案的功能
+     * 之所以用 commit 而不用 apply 是為了讓使用者知道是否開啟成功 或許會需要讓外部跳出提示訊息
+     * 而且此方法並不是會被頻繁呼叫的方法 寫入的資訊也非常小 故選擇 commit
+     *
+     * @param context
+     * @param isEnabled
+     * @return
+     */
+    public static boolean enableLogToFile(Context context, boolean isEnabled) {
+        SharedPreferences sp = PreferenceUtils.getPreference(context, LOG_CONFIG);
+        if (sp == null) {
+            LogUtils.w(TAG, "enableLogToFile(" + isEnabled + ") failed: sp is null");
+            return false;
+        }
+        if (sp.getBoolean(ENABLE_LOG_TO_FILE, false) != isEnabled) {
+            if (!sp.edit().putBoolean(ENABLE_LOG_TO_FILE, isEnabled).commit()) {
+                LogUtils.w(TAG, "enableLogToFile(" + isEnabled + ") failed: commit failed");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 如果這邊出現非預期的情況 並無法簡單回傳 true false 處理 因為這樣會造成外部混淆
+     * 所以這邊直接丟出例外
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isEnableLogToFile(Context context) {
+        SharedPreferences sp = PreferenceUtils.getPreference(context, LOG_CONFIG);
+        if (sp == null) {
+            LogUtils.e(TAG, "isEnableLogToFile() failed: sp is null");
+            throw new NullPointerException("isEnableLogToFile() error: sp is null");
+        }
+        return sp.getBoolean(ENABLE_LOG_TO_FILE, false);
     }
 }
 
